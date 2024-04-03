eb0eedbd0aea300414da45a8b15ec19ad29f6bf1
Carlos H.C.A.L <carlos.hcal@gmail.com>
2024-04-03 10:47:38 -0300

    Create script.js

---
+++ Introdução/script.js
@@ -0,0 +1,62 @@
+// Exercicio 1
+// Faça um programe qie exiba um decimal, um inteiro e uma string utilizando o console.log.
+
+const decimal = 3.14
+const inteiro = 5
+const palavra = "batata"
+
+console.log(decimal)
+console.log(inteiro)
+console.log(palavra)
+
+console.log(decimal, inteiro, palavra)
+
+// Exercicio 2
+// Declare uma variável, atribua um número, use o método typeof para mostrar na tela a palavra "number".
+
+const numero = 3
+
+console.log(typeof numero)
+
+// Exercicio 3
+// Declare uma variável, atribua um valor booleano, use o método typeof para mostrar na tela a palavra "boolean".
+
+const booleano = true
+
+console.log(typeof booleano)
+
+// Exercicio 4
+// Declare uma variável, atribua uma string, use o método typeof para mostrar na tela a palavra "string".
+
+const texto = "texto"
+
+console.log(typeof texto)
+
+// Exercicio 5
+// Faça um programa que exiba duas variáveis, uma com atribuição true e outra false.
+
+const verdadeiro = true
+const falso = false
+
+console.log( verdadeiro, falso)
+
+// Exercicio 6
+// Usando uma variável, e o console.log(), mostre no console o valor 'null'.
+
+const nulo = null
+
+console.log(nulo)
+
+// Exercicio 7
+// Usando uma variável, e o console.log(), mostre no console o valor 'undefined'.
+
+let indefinido
+
+console.log(indefinido)
+
+var soma = 1 + 10 + 9
+soma = 25
+soma = 20
+soma = 5 * (2 - 9)
+
+console.log(soma)
