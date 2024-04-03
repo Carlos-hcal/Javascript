e8876fd9ccc8d499c5009f77eca2220bcafe9abd
Carlos H.C.A.L <carlos.hcal@gmail.com>
2024-04-03 13:58:29 -0300

    Sintaxe e Declaração de Funções

---
+++ Funções/introdução.js
@@ -0,0 +1,68 @@
+//Função
+
+//Organizar um bloco de código em um conjunto fechado
+// Permitir que o código seja dividido em partes menores
+// Possibilitar reutilizar o código
+
+// const nome = "Thiago"
+// const sobrenome = "Koman"
+// const modulo = "M1"
+
+// console.log(`O nome completo do instrutor é: ${nome} ${sobrenome}, e ele trabalha no módulo ${modulo}`)
+
+// function formataApresentacao() {
+//     const nome = "Thiago"
+//     const sobrenome = "Koman"
+//     const modulo = "M1"
+
+//     console.log(`O nome completo do instrutor é: ${nome} ${sobrenome}, e ele trabalha no módulo ${modulo}`)
+// }
+
+// formataApresentacao()
+// formataApresentacao()
+// formataApresentacao()
+// formataApresentacao()
+// formataApresentacao()
+// formataApresentacao()
+// formataApresentacao()
+
+// const nome = "Lucas";
+// const sobrenome = "Jozefovicz";
+// const empresa = "Kenzie Academy Brasil";
+
+// console.log(`O nome completo é: ${nome} ${sobrenome}\nTrabalha na ${empresa}`);
+// // Saída: O nome completo é: Lucas Jozefovicz
+// //        Trabalha na Kenzie Academy Brasil
+
+// console.log(`O nome completo é: ${nome} ${sobrenome}\nTrabalha na ${empresa}`);
+// // Saída: O nome completo é: Lucas Jozefovicz
+// //        Trabalha na Kenzie Academy Brasil
+
+// console.log(`O nome completo é: ${nome} ${sobrenome}\nTrabalha na ${empresa}`);
+// // Saída: O nome completo é: Lucas Jozefovicz
+// //        Trabalha na Kenzie Academy Brasil
+
+// console.log(`O nome completo é: ${nome} ${sobrenome}\nTrabalha na ${empresa}`);
+// // Saída: O nome completo é: Lucas Jozefovicz
+// //        Trabalha na Kenzie Academy Brasil
+
+
+// Declaração da função que imprime nome completo e empresa
+function formataNomeEmpresa() {
+    const nome = "Lucas";
+    const sobrenome = "Jozefovicz";
+    const empresa = "Kenzie Academy Brasil";
+  
+    console.log(
+      `O nome completo é: ${nome} ${sobrenome}\nTrabalha na ${empresa}`
+    );
+  }
+  
+  // Execução da função declarada acima
+  formataNomeEmpresa();
+  formataNomeEmpresa();
+  formataNomeEmpresa();
+  formataNomeEmpresa();
+  // Será mostrado 4 vezes na tela:
+  // O nome completo é: Lucas Jozefovicz
+  // Trabalha na Kenzie Academy Brasil
---
+++ Funções/sintaxeEDeclaracaoDeFuncoes.js
@@ -0,0 +1,10 @@
+function nomeDaFuncao() {
+    //Logica da função
+}
+
+function somar() {
+    const numero1 = 3
+    const numero2 = 5
+
+    console.log(numero1 + numero2)
+}
