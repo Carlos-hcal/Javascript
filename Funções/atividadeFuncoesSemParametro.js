4771b84a45a17270c5445d64716db119dc4ca43e
Carlos H.C.A.L <carlos.hcal@gmail.com>
2024-04-04 08:35:59 -0300

    Atividade Funções Sem Parametro

---
+++ Funções/atividadeFuncoesSemParametro.js
@@ -0,0 +1,24 @@
+//Exercício 1
+//Desenvolva uma função que mostre no console a frase "Hello World!".
+
+//Exercício 2
+//Desenvolva uma função para saber se o(a) aluno(a) possui métricas para virar monitor(a).
+//Requisitos:
+
+//100 de Entregas
+//100 de Entrevistas Técnicas
+//100 de Presença
+//Se os requisitos forem atingidos, mostre no console "Possível monitor(a)." Se os requisitos não forem atingidos, mostre no console "Selecionar outro(a) aluno(a)."
+
+//Exercício 3
+//Desenvolva uma função que recebe três variáveis com valores numéricos e identifica qual deles é o maior.
+
+//Exercício 4
+//Desenvolva uma função para mostrar no console a seguinte frase: "Meu nome é -nome e sobrenome-, e tenho -idade- anos".
+
+//Declare as seguintes variáveis dentro da função:
+
+//nome
+//sobrenome
+//idade
+//Dica: Você pode utilizar template string.
